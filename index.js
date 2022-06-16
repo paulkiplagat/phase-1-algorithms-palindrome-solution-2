e43a921c4d15062a7213f237983c304d57f68663
Paul <paul.kiplagt@student.moringaschool.com>
2022-06-17 00:24:49 +0300

    commit tree c8ccda2ff9cdf387effbbe991aa72afd35fedf7b

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-function isPalindrome(word) {
   // iterate from the beginning of the string to the middle of the string
   for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
     // compare the letter we're iterating over to the corresponding letter at the end of the string
